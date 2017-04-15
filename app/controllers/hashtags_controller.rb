704fe7cf22538961a1de9564e1f2467591f0c01b
Hoan Nguyen <nguyenhoan2510@gmail.com>
2017-04-15 17:51:08 +0700

    Refactor hashtags controller

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -5,23 +5,34 @@ class HashtagsController < ApplicationController
   end
 
   def show
+    # Get hashtag after click
     @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag])
+
+    # Get all posts and comments contain that hashtag
     @hashtagged = @hashtag.hashtaggables if @hashtag
+
+    # It contains all posts that have hashtag
     @posts = []
-    @post_ids = []
+
+    # Use to check post_id has exist in array
+    post_ids = []
+
+    # Check hashtag belongs to Post or Comment
+    # If hashtag belongs to Comment (It contains post_id field)
+    # get post_id and post, after push into array posts
+    # If hashtag belongs to Post, just push that hashtag object into array posts
     @hashtagged.each do |hashtag|
       if hashtag.class.method_defined? :post_id
-        unless @post_ids.include? hashtag.post_id
-          @post_ids.push(hashtag.post_id)
+        unless post_ids.include? hashtag.post_id
+          post_ids.push(hashtag.post_id)
           @posts.push(hashtag.post)
         end
       else
-        unless @post_ids.include? hashtag.id
-          @post_ids.push(hashtag.id)
+        unless post_ids.include? hashtag.id
+          post_ids.push(hashtag.id)
           @posts.push(hashtag)
         end
       end
     end
   end
-
 end
