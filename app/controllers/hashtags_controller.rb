9c6252ce1ebd4facf5a94c1af260c20198f7e132
Hoan Nguyen <nguyenhoan2510@gmail.com>
2017-04-15 22:10:06 +0700

    Return nil array if not find hashtag Check display post after filter with hashtag and pagination

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -5,34 +5,32 @@ class HashtagsController < ApplicationController
   end
 
   def show
+    # Use to check post_id has exist in array
+    post_ids = []
+
     # Get hashtag after click
     @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag])
+    return Post.none if @hashtag.nil?
 
     # Get all posts and comments contain that hashtag
     @hashtagged = @hashtag.hashtaggables if @hashtag
 
-    # It contains all posts that have hashtag
-    @posts = []
-
-    # Use to check post_id has exist in array
-    post_ids = []
-
     # Check hashtag belongs to Post or Comment
     # If hashtag belongs to Comment (It contains post_id field)
-    # get post_id and post, after push into array posts
-    # If hashtag belongs to Post, just push that hashtag object into array posts
+    # get post_id and push into array
+    # If hashtag belongs to Post, just push that hashtag'id into array
     @hashtagged.each do |hashtag|
       if hashtag.class.method_defined? :post_id
         unless post_ids.include? hashtag.post_id
           post_ids.push(hashtag.post_id)
-          @posts.push(hashtag.post)
         end
       else
         unless post_ids.include? hashtag.id
           post_ids.push(hashtag.id)
-          @posts.push(hashtag)
         end
       end
     end
+
+    @posts = Post.where(id: post_ids).order(created_at: :desc).page params[:page]
   end
 end
