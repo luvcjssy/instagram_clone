4118fba29c171849a024aaa12a037aa4e827d165
Hoan Nguyen <nguyenhoan2510@gmail.com>
2017-04-15 19:29:05 +0700

    Format code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ActiveRecord::Base
-	include SimpleHashtag::Hashtaggable
+  include SimpleHashtag::Hashtaggable
+	
+  hashtaggable_attribute :title
 
-	hashtaggable_attribute :title
+  acts_as_votable
 
-	acts_as_votable
+  belongs_to :user
+  has_many :comments, dependent: :destroy
 
-	belongs_to :user
-	has_many :comments, dependent: :destroy
+  validates :user_id, presence: true
+  validates :image, presence: true
 
-	validates :user_id, presence: true
-	validates :image, presence: true
+  has_attached_file :image, styles: { medium: "300x300>", :large => "500x500>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-	has_attached_file :image, styles: { medium: "300x300>", :large => "500x500>" }
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-	crop_attached_file :image
+  crop_attached_file :image
 end
