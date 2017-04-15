6321885bead32c43057ac6d3ed81970c41f2d043
Hoan Nguyen <nguyenhoan2510@gmail.com>
2017-04-15 16:12:03 +0700

    Removed empty js files and comment functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery.jcrop
 //= require papercrop
@@ -20,11 +8,13 @@
 //= require jquery.turbolinks
 //= require_tree .
 
+/* Load image after choosing */
 var loadFile = function(event) {  
   var output = document.getElementById('image-preview');
   output.src = URL.createObjectURL(event.target.files[0]);
 };
 
+/* Load more comments */
 $( document ).ready(function() {
   $('.more-comments').click( function() {
     $(this).on('ajax:success', function(event, data, status, xhr) {
